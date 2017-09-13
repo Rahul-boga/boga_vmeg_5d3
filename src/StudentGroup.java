cd4f0f53d434d431a5cb8bff3398dbae0706b824
Rahul-boga <Rahul.rocks410@gmail.com>
2017-09-13 14:22:26 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -262,7 +262,19 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getByBirthDate(Date date) {
 		// Add your implementation here
-		return null;
+                if(date == null) {
+                    throw new IllegalArgumentException("not valid");
+                } else {
+                     ArrayList<Student> stu = new ArrayList<Student>();
+                    for(int i=0;i<this.students.length;i++) {
+                        if(this.students[i].getBirthDate().compareTo(date) == 0) {
+                            stu.add(this.students[i]);
+                        }
+                    }
+                   this.students = stu.toArray(new Student[stu.size()]);
+                    return this.students;
+                }
+		//return null;
 	}
 
 	@Override
