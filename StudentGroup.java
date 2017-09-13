47eb64a3469f4163f6a52fd1838c59207db8de52
Rahul-boga <Rahul.rocks410@gmail.com>
2017-09-13 14:27:49 +0530

    Add files via upload

--- StudentGroup.java
+++ StudentGroup.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.Date;
 
 /**
@@ -42,7 +43,7 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
-                if(index<0 || index>= students.length) {
+                if(index<0 || index>= this.students.length) {
                     throw new IllegalArgumentException("index out of bounds");
                 } else {
                     return this.students[index];
@@ -54,7 +55,7 @@ public class StudentGroup implements StudentArrayOperation {
 		// Add your implementation here
                 if(student == null){
                     throw new IllegalArgumentException("Student not exist");
-                } else if(index < 0 || index >= students.length) {
+                } else if(index < 0 || index >= this.students.length) {
                     throw new IllegalArgumentException("index out of bound");
                 } else {
                     this.students[index] = student;
@@ -70,7 +71,7 @@ public class StudentGroup implements StudentArrayOperation {
                 Student[] stu = new Student[this.students.length+1];
                 stu[0] = student;
                 for(int i=0;i<this.students.length;i++) {
-                    stu[i+1] = students[i];
+                    stu[i+1] = this.students[i];
                 }
                 this.students = stu;
                 }
@@ -85,7 +86,7 @@ public class StudentGroup implements StudentArrayOperation {
                 Student[] stu = new Student[this.students.length+1];
                 int i;
                 for(i=0;i<this.students.length;i++) {
-                    stu[i] = students[i];
+                    stu[i] = this.students[i];
                 }
                 stu[i+1] = student;
                 this.students = stu;
@@ -97,17 +98,17 @@ public class StudentGroup implements StudentArrayOperation {
 		// Add your implementation here
                 if(student == null) {
                     throw new IllegalArgumentException("student not exist");
-                } else if(index<0 || index>=students.length) {
+                } else if(index<0 || index>=this.students.length) {
                     throw new IllegalArgumentException("index out of bounds");
                 } else {
                     Student[] stu = new Student[this.students.length+1];
                     int i;
                     for(i=0;i<index;i++) {
-                        stu[i] = students[i];
+                        stu[i] = this.students[i];
                     }
                     stu[index] = student;
                     for(i=index+1;i<this.students.length;i++) {
-                        stu[i] = students[i-1];
+                        stu[i] = this.students[i-1];
                     }
                  
                 this.students = stu;
@@ -118,16 +119,16 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
-                if(index < 0 || index >= students.length ) {
+                if(index < 0 || index >= this.students.length ) {
                     throw new IllegalArgumentException("Student not exist");
                 } else {
                     Student[] stu = new Student[this.students.length-1];
                     int i,j=0;
-                    for(i=0;i<students.length;i++) {
+                    for(i=0;i<this.students.length;i++) {
                         if(i == index) {
                             continue;
                         } else {
-                            stu[j] = students[i];
+                            stu[j] = this.students[i];
                             j+=1;
                         }
                     }
@@ -138,17 +139,17 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void remove(Student student) {
 		// Add your implementation here
-                Student[] stu = new Student[students.length-1];
+                Student[] stu = new Student[this.students.length-1];
                 if(student == null) {
                     throw new IllegalArgumentException("Student not exist");
                 } else {
                     int flag = 0,j=0;
-                    for(int i=0;i<students.length;i++) {
-                        if(students[i] == student) {
+                    for(int i=0;i<this.students.length;i++) {
+                        if(this.students[i] == student) {
                             flag = 1;
                             continue;
                         } else {
-                            stu[j] = students[i];
+                            stu[j] = this.students[i];
                             j+=1;
                         }
                     }
@@ -163,16 +164,16 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void removeFromIndex(int index) {
 		// Add your implementation here
-                if(index<0 || index>=students.length) {
+                if(index<0 || index>=this.students.length) {
                     throw new IllegalArgumentException("index out of bounds");
                 } else {
                     //int j=0;
-                    Student[] stu = new Student[students.length-index];
-                    for(int i=0;i<students.length;i++) {
+                    Student[] stu = new Student[this.students.length-index];
+                    for(int i=0;i<this.students.length;i++) {
                         if(i == index+1) {
                             break;
                         } else {
-                            stu[i] = students[i];
+                            stu[i] = this.students[i];
                         }
                     }
                     this.students = stu;
@@ -186,8 +187,8 @@ public class StudentGroup implements StudentArrayOperation {
                     throw new IllegalArgumentException("student not exist");
                 } else {
                     int index=-1,i;
-                    for(i=0;i<students.length;i++) {
-                        if(students[i] == student) {
+                    for(i=0;i<this.students.length;i++) {
+                        if(this.students[i] == student) {
                           break;  
                         } 
                
@@ -195,9 +196,9 @@ public class StudentGroup implements StudentArrayOperation {
                     index = i;
                     Student[] stu = new Student[index];
                     for(i=0;i<=index;i++) {
-                        stu[i] = students[i];
+                        stu[i] = this.students[i];
                     }
-                    students = stu;
+                    this.students = stu;
                 }
                 
 	}
@@ -205,13 +206,13 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void removeToIndex(int index) {
 		// Add your implementation here
-                 if(index<0 || index>=students.length) {
+                 if(index<0 || index>=this.students.length) {
                     throw new IllegalArgumentException("index out of bounds");
                 } else {
                      Student[] stu = new Student[this.students.length-index];
                      int j=0;
-                     for(int i=index;i<students.length;i++) {
-                         stu[j] = students[i];
+                     for(int i=index;i<this.students.length;i++) {
+                         stu[j] = this.students[i];
                          j+=1;
                      }
                      this.students = stu;
@@ -226,8 +227,8 @@ public class StudentGroup implements StudentArrayOperation {
                     throw new IllegalArgumentException("student not exist");
                 } else {
                     int index=-1,i;
-                    for(i=0;i<students.length;i++) {
-                        if(students[i] == student) {
+                    for(i=0;i<this.students.length;i++) {
+                        if(this.students[i] == student) {
                           break;  
                         } 
                
@@ -235,8 +236,8 @@ public class StudentGroup implements StudentArrayOperation {
                     index = i;
                     int j=0;
                     Student[] stu = new Student[this.students.length-index];
-                    for(i=index;i<students.length;i++) {
-                        stu[j] = students[i];
+                    for(i=index;i<this.students.length;i++) {
+                        stu[j] = this.students[i];
                         j+=1;
                     }
                     this.students = stu;
@@ -247,12 +248,12 @@ public class StudentGroup implements StudentArrayOperation {
 	public void bubbleSort() {
 		// Add your implementation here
                 
-                for(int i=0;i<students.length-1;i++) {
-                    for(int j=0;j<students.length;j++) {
-                        if(students[j].getId() > students[j+1].getId()) {
-                            Student stu = students[j];
-                            students[j] = students[j+1];
-                            students[j+1] = stu;
+                for(int i=0;i<this.students.length;i++) {
+                    for(int j=0;j<this.students.length-i-1;j++) {
+                        if(this.students[j].getId() > this.students[j+1].getId()) {
+                            Student stu = this.students[j];
+                            this.students[j] = this.students[j+1];
+                            this.students[j+1] = stu;
                         }
                     }
                 }
@@ -261,13 +262,36 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getByBirthDate(Date date) {
 		// Add your implementation here
-		return null;
+                if(date == null) {
+                    throw new IllegalArgumentException("not valid");
+                } else {
+                     ArrayList<Student> stu = new ArrayList<Student>();
+                    for(int i=0;i<this.students.length;i++) {
+                        if(this.students[i].getBirthDate().compareTo(date) == 0) {
+                            stu.add(this.students[i]);
+                        }
+                    }
+                   this.students = stu.toArray(new Student[stu.size()]);
+                    return this.students;
+                }
+		//return null;
 	}
 
 	@Override
 	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
 		// Add your implementation here
-		return null;
+                if(firstDate == null || lastDate == null) {
+                    throw new IllegalArgumentException("not valid");
+                } else {
+                    ArrayList<Student> stu = new ArrayList<Student>();
+                    for(int i=0;i<this.students.length;i++) {
+                        if(this.students[i].getBirthDate().after(firstDate) && this.students[i].getBirthDate().before(lastDate)) {
+                            stu.add(this.students[i]);
+                        }
+                    }
+                   this.students = stu.toArray(new Student[stu.size()]);
+                    return this.students;
+                }
 	}
 
 	@Override
@@ -291,7 +315,23 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getStudentsWithMaxAvgMark() {
 		// Add your implementation here
-		return null;
+                double maxMark=0;
+                int i;
+                
+                for(i=0;i<this.students.length;i++) {
+                    if(this.students[i].getAvgMark() > maxMark) {
+                        maxMark = this.students[i].getAvgMark();
+                        //count+=1;
+                    }
+                }
+                ArrayList<Student> al = new ArrayList<Student>();
+                for(i=0;i<this.students.length;i++) {
+                    if(this.students[i].getAvgMark() == maxMark) {
+                        al.add(this.students[i]);
+                    } 
+                }
+                this.students = al.toArray(new Student[al.size()]);
+		return this.students;
 	}
 
 	@Override
@@ -301,12 +341,12 @@ public class StudentGroup implements StudentArrayOperation {
                     throw new IllegalArgumentException("student not exist");
                 } else {
                    int i;
-                    for(i=0;i<students.length;i++) {
-                        if(students[i] == student) {
+                    for(i=0;i<this.students.length;i++) {
+                        if(this.students[i] == student) {
                             break;
                         }
                     }
-                    return students[i+1];
+                    return this.students[i+1];
                 }
 		//return null;
 	}
